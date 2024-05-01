afa1ef33932f5d5a21536ec87c08edb588868b9a
Yakub Ali <mamuzaan@yahoo.com>
2024-05-01 20:41:33 +0000

    Added django allauth template site packages

--- rdredovisning/settings.py
+++ rdredovisning/settings.py
@@ -152,3 +152,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'  # Or 'https' if your site uses HTTPS
